1001a83044c862b22b0d070460bcf236977d67d5
Augustin VERLANDE <augustin.verlande@etu.univ-lyon1.fr>
2024-09-25 22:05:45 +0200

    - Ajout d'une hashmap pour stocker les stackpanes de chaque élément de la grille - Ajout de la fonction updateCellule (met à jour la position des véhicules sur la grille)

--- Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/controllers/TerrainController.java
+++ Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/controllers/TerrainController.java
@@ -11,12 +11,16 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class TerrainController implements Initializable {
     @FXML
     private GridPane grilleInitiale;
     private Terrain terrain;
+
+    private Map<Vector2D, StackPane> mapStackPanes = new HashMap<>(); // stackspanes de chaque élément de la grille
 
     public TerrainController(Simulation s) { //constructeur
         terrain = s.getTerrain();
@@ -29,16 +33,21 @@ public class TerrainController implements Initializable {
         this.terrain = terrain;
     }
 
-    private void dessinerGrille() {
+    private void dessinerGrille()
+    {
         Cellule[][] grille = terrain.getGrille();
         int tailleCellule = 10; //param fixe en dehors ??
 
         // Vider la grille avant de dessiner si elle contient déjà des éléments
         grilleInitiale.getChildren().clear();
 
-        for (int i = 0; i < terrain.getLargeur(); i++) {
-            for (int j = 0; j < terrain.getHauteur(); j++) {
+        // Vider la map si elle contient déjà des éléments
+        mapStackPanes.clear();
 
+        for (int i = 0; i < terrain.getLargeur(); i++)
+        {
+            for (int j = 0; j < terrain.getHauteur(); j++)
+            {
                 // Créer une StackPane pour chaque cellule
                 StackPane stackPane = new StackPane();
 
@@ -46,13 +55,16 @@ public class TerrainController implements Initializable {
                 Rectangle rect = new Rectangle(tailleCellule,tailleCellule);
                 setCouleurCellule(rect, grille[i][j]);
 
-                //rect.setStroke(Color.BLACK);  // Bordure noire pour mieux voir les cellules
                 stackPane.getChildren().add(rect);
 
                 grilleInitiale.add(stackPane, i, j); // Ajouter la stack pane à la position (i , j) de la grille
+
+                Vector2D position = new Vector2D(i, j); // ajout de la stackpane dans la map
+                mapStackPanes.put(position, stackPane);
             }
         }
     }
+
     // Définir la couleur de la cellule selon son type
     private void setCouleurCellule(Rectangle rect, Cellule cellule) {
         if (cellule.estValide()) {
@@ -66,11 +78,18 @@ public class TerrainController implements Initializable {
             rect.setFill(Color.GREEN); // Espace vide
         }
     }
-    public void updateCellule(Vector2D anciennePosition, Vector2D nouvellePosition){
-        /** Implémenter ici comment une case change d'état en fonction du déplacement de la voiture
-         idée : on rajoute un paramètre Map<Vector2D,Stackpane> comme ça on garde pour chaque position de la grille la stackpane associée
-         pour éviter de redessiner la grille à chaque déplacement de chaque voiture
-         théoriquement la stackpane va s'update toute seule sur l'écran, mais à revérifier
-         **/
+
+    public void updateCellule(Vector2D anciennePosition, Vector2D nouvellePosition, Rectangle vehiculeRectangle) {
+        // retirer le véhicule à l'ancienne position
+        StackPane ancienneCellule = mapStackPanes.get(anciennePosition);
+        if (ancienneCellule != null) {
+            ancienneCellule.getChildren().remove(vehiculeRectangle); // suppression
+        }
+
+        // ajouter la voiture
+        StackPane nouvelleCellule = mapStackPanes.get(nouvellePosition);
+        if (nouvelleCellule != null) {
+            nouvelleCellule.getChildren().add(vehiculeRectangle); // affichage
+        }
     }
 }
