8a2365caa796511a1f5d555ae72d33e146b8f511
Augustin VERLANDE <augustin.verlande@etu.univ-lyon1.fr>
2024-10-12 21:05:40 +0200

    Agrandissement de la fenêtre de simulation
    Tests de communication commentés

--- Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/MainTrafficApplication.java
+++ Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/MainTrafficApplication.java
@@ -19,6 +19,7 @@ public class MainTrafficApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         Simulation simulation = new Simulation();
+        Terrain terrain = simulation.getTerrain();
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainTrafficApplication.class.getResource("Terrain.fxml"));
         fxmlLoader.setControllerFactory(obj -> new TerrainController(simulation));
@@ -29,38 +30,38 @@ public class MainTrafficApplication extends Application {
         // Passer l'instance de TerrainController à la simulation
         simulation.setTerrainController(terrainController);
 
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, 525, 525);
         stage.setTitle("Mon Terrain");
         stage.setScene(scene);
         stage.show();
 
         simulation.lancerSimulation();
-        // Création des véhicules
-        Vehicule v1 = new Vehicule(TypeVehicule.VOITURE, new Vector2D(3, 4), new Vector2D(4, 5));
-        Vehicule v2 = new Vehicule(TypeVehicule.VOITURE, new Vector2D(6, 7), new Vector2D(7, 8));
-        Vehicule v3 = new Vehicule(TypeVehicule.VOITURE, new Vector2D(5, 5), new Vector2D(6, 6));
-
-// Ajoutez v2 et v3 comme écouteurs pour v1 (pour recevoir les messages de v1)
-        v1.addListener(v2);
-        v1.addListener(v3);
-
-// Envoi d'un message depuis v1
-        Vector2D vec1 = new Vector2D(1, 1);
-        Vector2D vec2 = new Vector2D(2, 2);
-        ArrayList<Vector2D> itineraire = new ArrayList<>();
-        itineraire.add(vec1);
-        itineraire.add(vec2);
-
-// Liste des destinataires (recipients)
-        ArrayList<Vehicule> recipients = new ArrayList<>();
-        recipients.add(v2); // Destinataire
-        recipients.add(v3); // Destinataire
-
-// Création du message
-        Message message = new Message(1, v1, recipients, Objetmessage.PASSAGE, itineraire);
-
-// Envoi du message
-        v1.sendMessage(message);
+//        // Création des véhicules
+//        Vehicule v1 = new Vehicule(TypeVehicule.VOITURE, new Vector2D(3, 4), new Vector2D(4, 5), terrain);
+//        Vehicule v2 = new Vehicule(TypeVehicule.VOITURE, new Vector2D(6, 7), new Vector2D(7, 8), terrain);
+//        Vehicule v3 = new Vehicule(TypeVehicule.VOITURE, new Vector2D(5, 5), new Vector2D(6, 6), terrain);
+//
+//// Ajoutez v2 et v3 comme écouteurs pour v1 (pour recevoir les messages de v1)
+//        v1.addListener(v2);
+//        v1.addListener(v3);
+//
+//// Envoi d'un message depuis v1
+//        Vector2D vec1 = new Vector2D(1, 1);
+//        Vector2D vec2 = new Vector2D(2, 2);
+//        ArrayList<Vector2D> itineraire = new ArrayList<>();
+//        itineraire.add(vec1);
+//        itineraire.add(vec2);
+//
+//// Liste des destinataires (recipients)
+//        ArrayList<Vehicule> recipients = new ArrayList<>();
+//        recipients.add(v2); // Destinataire
+//        recipients.add(v3); // Destinataire
+//
+//// Création du message
+//        Message message = new Message(1, v1, recipients, Objetmessage.PASSAGE, itineraire);
+//
+//// Envoi du message
+//        v1.sendMessage(message);
 
     }
 
