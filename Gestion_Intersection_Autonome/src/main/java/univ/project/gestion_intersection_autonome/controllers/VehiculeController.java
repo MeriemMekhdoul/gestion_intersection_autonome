2f62aad2fed23df59e14864cbadc5da45bfa566c
Meriem Mekhdoul <meriem.mekhdoul@etu.univ-lyon1.fr>
2024-10-12 23:51:54 +0200

    mise à jour de la logique de passage dans l'intersection + utiliser le tableau d'itinéraire prêt

--- Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/controllers/VehiculeController.java
+++ Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/controllers/VehiculeController.java
@@ -19,8 +19,7 @@ public class VehiculeController implements Runnable {
     private Vector2D nouvellePosition;
     private final Shape vehiculeShape; // référence de la forme du véhicule
     private boolean entreeIntersection = true; // pour savoir si on rentre ou on sort d'une intersection
-    private boolean majAffichageFaite = false;
-    private final int VITESSE_SIMULATION_MS = 100;
+    private final int VITESSE_SIMULATION_MS = 1000;
 
     public VehiculeController(Vehicule vehicule, Terrain terrain, TerrainController terrainController) {
         this.vehicule = vehicule;
@@ -33,61 +32,34 @@ public class VehiculeController implements Runnable {
     public void run() {
         List<Vector2D> itineraire = vehicule.getItineraire();
 
-        // on démarre à 1 car 0 est la position de départ (actuelle)
+        // on démarre à 1, car 0 est la position de départ (actuelle)
         for (int i = 1; i < itineraire.size(); i++)
         {
             anciennePosition = vehicule.getPosition().copy();
             nouvellePosition = itineraire.get(i);
 
-            System.out.println("Le véhicule " + vehicule.getId() + " se déplace vers : " + vehicule.getPosition() + " en direction de " + vehicule.getPositionArrivee());
-
-            // commenté car lorsqu'un véhicule quitte le terrain, sa dernière position est toujours occupée, voir pour régler ça
-
             // vérification de l'occupation de la cellule
-//            if (terrain.getCellule(nouvellePosition).estOccupee())
-//            {
-//                System.out.println("Cellule occupée");
-//
-                  // attente libération
-//                while (terrain.getCellule(nouvellePosition).estOccupee()) {
-//                    pauseEntreMouvements(VITESSE_SIMULATION_MS);
-//                    System.out.println("Attente libération cellule");
-//                }
-//            }
+            if (terrain.getCellule(nouvellePosition).estOccupee())
+            {
+                System.out.println("Cellule occupée");
 
-            vehicule.move(nouvellePosition);
-            mettreAJourCellules();
-            mettreAJourGraphique();
-            pauseEntreMouvements(VITESSE_SIMULATION_MS);
-
-            if (vehicule.estArrivee()) {
-                System.out.println("Le véhicule " + vehicule.getId() + " est arrivé à destination !");
-                break;
+                // attente libération
+                while (terrain.getCellule(nouvellePosition).estOccupee()) {
+                    pauseEntreMouvements(VITESSE_SIMULATION_MS);
+                    System.out.println("Attente libération cellule");
+                }
             }
-        }
 
-        Platform.runLater(() -> {
-            terrainController.effacerVehicule(nouvellePosition, vehiculeShape);
-            System.out.println("Véhicule " + vehicule.getId() + " effacé");
-            terrainController.getSimulation().supprimerVehicule(vehicule, this);
-            System.out.println("Véhicule " + vehicule.getId() + " supprimé");
-        });
-    }
-
-    /*@Override
-    public void run()
-    {
-        while (!vehicule.estArrivee())
-        {
             deplacement();
-
-            if (!majAffichageFaite) {
-                mettreAJourGraphique();
-                pauseEntreMouvements(VITESSE_SIMULATION_MS);
-            } else {
-                majAffichageFaite = false;
-            }
+            //mettre l'index à jour dans le cas du déplacement dans l'intersection
+            i = itineraire.indexOf(nouvellePosition);
+            pauseEntreMouvements(VITESSE_SIMULATION_MS);
         }
+
+        //libérer la dernière cellule occupée
+        Cellule cell = terrain.getCellule(nouvellePosition);
+        cell.setOccupee(false);
+        cell.setIdVoiture(0);
 
         System.out.println("Le véhicule " + vehicule.getId() + " est arrivé à destination !");
 
@@ -98,23 +70,16 @@ public class VehiculeController implements Runnable {
             System.out.println("Véhicule " + vehicule.getId() + " supprimé");
         });
     }
-    */
 
-    public void deplacement() {
-        anciennePosition = vehicule.getPosition().copy();
 
-        // if cell1 est communication appeler gestion intersection sinon exécuter normalement
-        // ET si on va entrer dans une intersection et pas en sortir
+    public void deplacement()
+    {
         if(estDansCommunication(anciennePosition) && entreeIntersection){
-            System.out.println("je suis dans le if comm");
-            //appeler gestion intersection qui va generer un tableau de déplacements,
-            //si tout va bien elle va avancer jusqu'à sortir de l'intersection
+            System.out.println("je suis dans une zone de communication");
             entrerIntersection();
-            //colorier les cases (appel fonction dans terrainController et lui passer le tableau des deplacements
-            majAffichageFaite = true; //j'ai mis à jour les rectangles
             entreeIntersection = false; //je suis sortie de l'intersection
         } else {
-            System.out.println("je suis dans le else comm");
+            System.out.println("je suis en zone normale");
             deplacerHorsIntersection();
             //mettre à jour l'attribut "entree" pour savoir si on arrive de nouveau dans une intersection ou pas
             if (estDansCommunication(nouvellePosition)) {
@@ -124,15 +89,14 @@ public class VehiculeController implements Runnable {
         }
     }
 
-    private void deplacerHorsIntersection() {
+    private void deplacerHorsIntersection()
+    {
         System.out.println("Déplacement hors intersection");
-        /** Il faut vérifier que la case ou on va ne dépasse pas le tableau **/
-        List<Vector2D> cellulesPotentielles = getCellulesAutour(vehicule.getPosition());
-        Vector2D positionSuivante = vehicule.choisirPositionOptimale(cellulesPotentielles);
-        vehicule.move(positionSuivante);
-        // Afficher les informations de déplacement
+        vehicule.move(nouvellePosition);
+
         System.out.println("Le véhicule " + vehicule.getId() + " se déplace vers : " + vehicule.getPosition());
         mettreAJourCellules();
+        mettreAJourGraphique();
     }
 
     private void entrerIntersection() {
@@ -148,32 +112,160 @@ public class VehiculeController implements Runnable {
         message.setv1(vehicule);
         message.setItineraire(deplacements);
 
-        intersection.addV(vehicule,message); //l'ajouter a la config
+        intersection.ajouterVehicule(vehicule,message); //l'ajouter a la config
 
-        ArrayList<Vehicule> vehiculesDestinataires = intersection.getVehiculesEnAttente(); //les véhicules qui ne sont pas engagés
-        if (vehiculesDestinataires.isEmpty()){
-            System.out.println("aucun vehicule en attente donc j'entre dans l'intersection");
+        ArrayList<Vehicule> vehiculesEngages = intersection.getVehiculesEngages(); //les véhicules qui ne sont pas engagés
+        ArrayList<Vehicule> vehiculesDansIntersection = intersection.getVehicules();
+
+        if (vehiculesDansIntersection.isEmpty()) {
             //send message "Engagée" ????
             intersection.editConfig(vehicule,EtatVehicule.ENGAGE);
+            System.out.println("aucun vehicule dans l'intersection donc j'avance'");
             avancerIntersection(deplacements);
             //quand on arrive a la fin (la sortie de la zone) on envoie un message de sortie et on supprime l'objet vehicule de la config de l'intersection
             //si on envoie un msg de sortie à l'intersection, ça sera un signal pour supprimer la voiture de sa config et ne pas faire l'action içi
-            intersection.suppV(vehicule);
-            System.out.println("j'ai supp le vehicule de la config");
-        } else {
-            message.setv2(vehiculesDestinataires);
-            //entrer dans le mode négociation, calculs et gestion des priorités
+            intersection.supprimerVehicule(vehicule);
+            System.out.println("je suis sorti de l'intersection et j'ai supp le vehicule de la config");
+        }
+        else //entrer dans le mode négociation, calculs et gestion des priorités
+        {
+            //récupérer les infos (itinéraires) des autres
+            ArrayList<Message> messagesVoitures = new ArrayList<>();
+            for (Vehicule v: vehiculesDansIntersection) {
+                messagesVoitures.add(intersection.getMessage(v));
+            }
+
+            //ArrayList<Vehicule> vehiculesEnConflit = new ArrayList<>();
+            //if (conflit(messagesVoitures,deplacements,vehiculesEnConflit))
+            //{
+                //System.out.println("il y a un conflit avec certains véhicules");
+
+                //set up les listeners
+                //liaisonListeners(vehiculesEnConflit);
+
+                System.out.println("je calcule mon temps d'attente");
+                int tempsAttente = calculs(messagesVoitures,deplacements,vehiculesEngages);
+/*
+                //Créer un nouveau message et l'envoyer aux véhicules en conflit
+                Message messageConfig = new Message();
+                messageConfig.setv1(vehicule);
+                messageConfig.setv2(vehiculesEnConflit);
+                messageConfig.setObjet(Objetmessage.CONFIG);
+                messageConfig.setConfiguration(configProposee);
+
+                vehicule.sendMessage(messageConfig);
+*/
+            System.out.println("temps d'attente calculé et estimé à : " + tempsAttente + " secondes");
+            pauseEntreMouvements(tempsAttente * VITESSE_SIMULATION_MS);
+            System.out.println("attente effectuée, conflit évité, update l'état à ENGAGE puis j'avance");
+
+            //} else
+            //{
+                //System.out.println("aucun conflit avec les autres voitures, je peux avancer directement");
+                //changer l'état de la voiture dans la config de l'intersection
+                intersection.editConfig(vehicule,EtatVehicule.ENGAGE);
+                //Envoyer un message avant de s'engager ??
+
+                avancerIntersection(deplacements);
+                intersection.supprimerVehicule(vehicule);
+                //à la sortie envoyer un msg de SORTIE (à qui ??) => intersection ou véhiculesDestinataires ?
+            //}
+
         }
     }
 
-    private boolean estDansCommunication(Vector2D position) {
+    /**
+     * La fonction vérifie s'il y aura un potentiel conflit dans l'intersection.
+     * Si un conflit est détecté, retourne `true` et met à jour la liste des véhicules impliqués dans le conflit.
+     * Sinon, retourne `false` et la liste reste vide.
+     *
+     * @param messagesReçus Liste des messages contenant les informations sur les véhicules et leurs itinéraires.
+     * @param itineraire L'itinéraire du véhicule actuel.
+     * @param vehiculesEnConflit Liste des véhicules qui causent un conflit (mise à jour si conflit détecté).
+     * @return `true` s'il y a un conflit, sinon `false`.
+     */
+    public boolean conflit(ArrayList<Message> messagesReçus, ArrayList<Vector2D> itineraire, ArrayList<Vehicule> vehiculesEnConflit)
+    {
+        // Vider la liste des véhicules en conflit pour un nouveau calcul
+        vehiculesEnConflit.clear();
+
+        // Récupérer un tableau des itinéraires depuis les messages
+        for (Message message : messagesReçus) {
+            ArrayList<Vector2D> itineraireAutreVehicule = message.getItineraire();
+
+            // Si une collision est détectée entre les itinéraires
+            if (compareItineraire(itineraire, itineraireAutreVehicule)) {
+                // Ajouter le véhicule en conflit à la liste
+                vehiculesEnConflit.add(message.getv1());
+            }
+        }
+        // Si des véhicules en conflit sont détectés, retourner vrai
+        return !vehiculesEnConflit.isEmpty();
+    }
+
+    /**
+     * Compare deux itinéraires pour détecter une éventuelle collision. (à renommer en détécterCollision())
+     * @param itin1 Le premier itinéraire.
+     * @param itin2 Le second itinéraire.
+     * @return `true` s'il y a une collision, sinon `false`.
+     */
+    public boolean compareItineraire(ArrayList<Vector2D> itin1, ArrayList<Vector2D> itin2)
+    {
+        for (int i = 0; i < itin1.size(); i++) {
+            if (i< itin2.size()) {
+                if (itin1.get(i).equals(itin2.get(i)))
+                    return true;
+            } else return false;
+        }
+        return false; //pas de collision
+    }
+
+    /**
+     * Retourne le temps d'attente du véhicule en fonction des itinéraires des autres véhicules
+     *
+     * @param messagesReçus Liste des messages reçus contenant les itinéraires des autres véhicules.
+     * @param itineraire L'itinéraire de mon véhicule.
+     * @param vehiculesEngages Les véhicules qui sont déjà engagés dans l'intersection
+     * @return Une configuration optimisée de l'ordre de passage des véhicules.
+     */
+    public int calculs(ArrayList<Message> messagesReçus, ArrayList<Vector2D> itineraire, ArrayList<Vehicule> vehiculesEngages)
+    {
+        int tempsAttente = 0;
+        // Filtrer les messages pour ne garder que ceux qui concernent les véhicules en conflit
+        ArrayList<Message> messagesFiltres = new ArrayList<>();
+
+        // Proposer un ordre de passage en tenant compte des itinéraires filtrés
+        for (Message message : messagesFiltres) {
+            ArrayList<Vector2D> itineraireVehicule = message.getItineraire();
+
+            //////////// A IMPLEMENTER //////////////////
+        }
+
+        return tempsAttente;
+    }
+    public void liaisonListeners(ArrayList<Vehicule> destinataires)
+    {
+        for (Vehicule v: destinataires) {
+            vehicule.addListener(v);
+        }
+    }
+
+    private boolean estDansCommunication(Vector2D position)
+    {
         return terrain.getCellule(position).getTypeZone() == TypeZone.COMMUNICATION;
     }
 
-// Retournes les positions des cellules ou la voiture peut se déplacer en vérifiant leur accessibilité et ne pas faire marche arrière
+    /**
+     * Retourne une liste des positions des cellules voisines accessibles à partir d'une position donnée,
+     * en fonction des directions autorisées et en évitant le retour en arrière.
+     *
+     * @param positionActuelle La position actuelle du véhicule représentée par un objet `Vector2D`.
+     * @return Une liste de positions (objets `Vector2D`) des cellules accessibles à partir de la position actuelle.
+     */
     public List<Vector2D> getCellulesAutour(Vector2D positionActuelle)
     {
         List<Vector2D> cellulesPotentielles = new ArrayList<>();
+
         Cellule celluleActuelle = terrain.getGrille()[positionActuelle.getX()][positionActuelle.getY()];
         boolean[] directionsAutorisees = celluleActuelle.getDirectionsAutorisees();
 
@@ -194,21 +286,27 @@ public class VehiculeController implements Runnable {
         return cellulesPotentielles;
     }
     
-    public ArrayList<Vector2D> gestionIntersection(){
+    public ArrayList<Vector2D> gestionIntersection()
+    {
+        List<Vector2D> itineraire = vehicule.getItineraire();
         ArrayList<Vector2D> deplacements = new ArrayList<>();
+
+        int index = itineraire.indexOf(anciennePosition) + 1;
+
         //tant que je ne suis pas dans une cellule de communication je continue sinon j'arrête et je renvoie mon tableau
-        List<Vector2D> cellulesPotentielles = getCellulesAutour(vehicule.getPosition());
-        Vector2D posSuivante = vehicule.choisirPositionOptimale(cellulesPotentielles);
+        Vector2D posSuivante = itineraire.get(index);
         deplacements.add(posSuivante);
+
         while (!estDansCommunication(posSuivante)) {
-            cellulesPotentielles = getCellulesAutour(posSuivante);
-            posSuivante = vehicule.choisirPositionOptimale(cellulesPotentielles);
+            index++;
+            posSuivante = itineraire.get(index);
             deplacements.add(posSuivante);
         }
         return deplacements;
     }
 
-    private void avancerIntersection(List<Vector2D> deplacements) {
+    private void avancerIntersection(List<Vector2D> deplacements)
+    {
         for (Vector2D pos : deplacements) {
             anciennePosition = vehicule.getPosition().copy();
             vehicule.move(pos);
@@ -220,7 +318,8 @@ public class VehiculeController implements Runnable {
         }
     }
 
-    public void mettreAJourCellules(){
+    public synchronized void mettreAJourCellules()
+    {
         nouvellePosition = vehicule.getPosition().copy();
         Cellule cell2 = terrain.getCellule(nouvellePosition);
         Cellule cell1 = terrain.getCellule(anciennePosition);
@@ -231,11 +330,13 @@ public class VehiculeController implements Runnable {
         cell2.setIdVoiture(vehicule.getId());
     }
 
-    private void mettreAJourGraphique() {
+    private void mettreAJourGraphique()
+    {
         Platform.runLater(() -> terrainController.updateCellule(anciennePosition, nouvellePosition, vehiculeShape));
     }
 
-    private void pauseEntreMouvements(int millisecondes) {
+    private void pauseEntreMouvements(int millisecondes)
+    {
         try {
             Thread.sleep(millisecondes);
         } catch (InterruptedException e) {
