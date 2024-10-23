5dd1e75b85fe410ae78d234262bf7a5e62ab3ad1
Meriem Mekhdoul <meriem.mekhdoul@etu.univ-lyon1.fr>
2024-10-23 22:34:52 +0200

    ajout de nouveaux types de messages (pour la communication avec le véhicule d'urgence)

--- Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/classes/Objetmessage.java
+++ Gestion_Intersection_Autonome/src/main/java/univ/project/gestion_intersection_autonome/classes/Objetmessage.java
@@ -5,5 +5,6 @@ public enum Objetmessage {
     INFORMATION, //pour informer de l'arrivée du véhicule dans une zone de comm - renommer en ENTREE ?
     TRAFIC, //information sur le trafic dans les autres voies
     CONFIG, //proposition d'une configuration de passage lors d'un conflit
-    SORTIE  //sortie du véhicule de l'intersection
+    SORTIE,  //sortie du véhicule de l'intersection
+    MARCHE, STOP    //demande d'arrêt de l'intersection à un véhicule
 }
